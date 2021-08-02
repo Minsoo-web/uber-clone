d3b307f16aeec61ed4f6426ac3076ea001397ff7
minsoo kim <zlemzlem5656@naver.com>
2021-08-03 00:30:20 +0900

    [수정] GCP API 연동

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,14 +3,51 @@ import { Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEYS } from "@env";
+import { useDispatch } from "react-redux";
+import { selectDestination, selectOrigin, setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
         <Image
           style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={{ uri: "https://links.papareact.com/gzs" }}
+        />
+
+        <GooglePlacesAutocomplete
+          styles={{
+            container: {
+              flex: 0
+            },
+            textInput: {
+              fontSize: 18
+            }
+          }}
+          minLength={2}
+          enablePoweredByContainer={false}
+          placeholder="where from ?"
+          onPress={(data, details = null) => {
+            // console.log(data);
+            console.log(details.geometry);
+            // dispatch(
+            //   setOrigin({
+            //     location: details?.geometry.location,
+            //     description: data.description
+            //   })
+            // );
+            // dispatch(setDestination(null));
+          }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          query={{
+            key: GOOGLE_MAPS_APIKEYS,
+            language: "en"
+          }}
         />
         <NavOptions />
       </View>
